1ed9306d077cf2f0b1c41fb06b9e967c0aa522bb
Amar Dhillon <87444806+AmarDhillon05@users.noreply.github.com>
2025-06-29 23:51:13 -0400

    Update Skills.js

--- src/routes/Skills.js
+++ src/routes/Skills.js
@@ -11,20 +11,20 @@ export default function Home(){
                 <Skills name = "Languages" 
                 skills = {["Python", "Javascript", "HTML/CSS", "Java", "C", "C++", "C#", "R"]}></Skills>
 
-                <Skills name = "Web" 
+                <Skills name = "Web & App Development" 
                 skills = {["React", "Vue", "Express", "Flask", "Django", "Next"]}></Skills>
 
-                <Skills name = "Data" 
+                <Skills name = "Data and Analytics" 
                 skills = {["SQL", "MongoDB", "Excel", "Tableau", "Pandas", "NumPy", "OpenCV", "GraphQL"]}></Skills>
 
-                <Skills name = "AI/ML" 
-                skills = {["SkLearn", "Tensorflow", "PyTorch", "HuggingFace", "LLMs", "Pinecone", "Faiss"]}></Skills>
+                <Skills name = "AI & Machine Learning" 
+                skills = {["SkLearn", "Tensorflow", "PyTorch", "HuggingFace", "LangChain", "Pinecone", "Faiss"]}></Skills>
 
-                <Skills name = "Hardware" 
-                skills = {["Arduino", "Rasberry Pi", "STM32", "Assembly", "PID", "Sensor Integration"]}></Skills>
+                <Skills name = "Hardware & Embedded" 
+                skills = {["Arduino", "Rasberry Pi", "STM32", "Assembly", "FreeRTOS", "Sensor Integration"]}></Skills>
 
-                <Skills name = "Other" 
-                skills = {["Git/Github", "Web Scraping", "Linux", "AWS", "Docker"]}></Skills>
+                <Skills name = "Cloud & DevOps" 
+                skills = {["AWS", "Docker", "GCP", "Terraform", "Git", "CICD"]}></Skills>
 
             </div>
 
